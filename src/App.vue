faf5c7cf714130458efdf59ff6112a12983ab393
Gabriel_Silva <gabriel.ieda@hotmail.com>
2022-05-16 17:26:54 -0300

    - Usando v-model - Passagem de bnome por components

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <template>
   <div>
     <!-- <router-link class="link" :to="{ path: '/user', query: { name: 'Foo' }}">Say hello</router-link> -->
-    <router-view />
-    <AgifyApi />
+    <!-- <router-view /> -->
+    <!-- <AgifyApi /> -->
+    <h1>APP: {{ bnome }}</h1>
+    <Usuario v-model:bnome="bnome" />
   </div>
 </template>
 
-<script setup>
+<script>
 import AgifyApi from "./components/AgifyApi.vue";
+import Usuario from "./components/Usuario.vue";
+export default {
+  components: { Usuario },
+  data() {
+    return {
+      bnome: "v-model argument example",
+    };
+  },
+};
 </script>
--- src/components/Usuario.vue
+++ src/components/Usuario.vue
@@ -1,17 +1,43 @@
 <template>
   <div>
-    <h1>Start: {{ msg }}</h1>
+    <h1>Usuario: {{ bnome }}</h1>
+    <input
+      type="text"
+      :value="bnome"
+      @input="$emit('update:bnome', $event.target.value)"
+    />
+    <button v-on:click="teste">Teste</button>
   </div>
 </template>
 
 <script>
+import { defineComponent, onMounted, ref } from "@vue/runtime-core";
+import api from "../services/api";
+import router from "@/routes/router.js";
+
 export default {
-  name: "Buscar",
-  props: ["name"],
-  computed: {
-    msg() {
-      console.log(this.name);
-      return `Ola, ${this.name}!!`;
+  name: "Usuario",
+  props: ["bnome"],
+  emits: ["update:bnome"],
+  watch: {
+    bnome: function (newVal, oldVal) {
+      // watch it
+      console.log("Prop changed: ", newVal, " | was: ", oldVal);
+    },
+  },
+  methods: {
+    teste() {
+      var nome = this.bnome;
+      const usuario = ref([]);
+      let fetchUsuario = () =>
+        api
+          .get("/?name=" + nome)
+          .then((resp) => (usuario.value = resp.data))
+          .catch((err) => console.log(err));
+      // onMounted(fetchUsuario);
+      console.warn(fetchUsuario);
+      console.warn(nome);
+      return { usuario };
     },
   },
 };
